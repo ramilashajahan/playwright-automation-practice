693a4adc04af1728de48f9da4c125be2a1662ba2
ramilashajahan <ramilashajahan@gmail.com>
2026-07-06 21:37:48 +0530

    Create playwright.config.ts

---
+++ playwright.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  timeout: 30000,
+  expect: { timeout: 5000 },
+  fullyParallel: true,
+  reporter: 'html',
+  use: {
+    baseURL: 'https://practice.expandtesting.com', // A standard safe practice website
+    trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+  },
+  projects: [
+    { name: 'chromium', use: { ...devices['Desktop Chrome'] } },
+    { name: 'firefox', use: { ...devices['Desktop Firefox'] } }
+  ],
+});
